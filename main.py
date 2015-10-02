2ebff5e0a0149df6223c6cecfc91a07178577cbf
Bacas Kevin <bacas@ecole.ensicaen.fr>
2015-10-02 13:48:40 +0200

    Added bezout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from utils import pgcd
+from utils import pgcd, bezout
 
 if __name__ == '__main__':
 	print "Hello world!"
 	print "pgcd(6, 9) should equal 3 :", pgcd(6, 9)
 	print "pgcd(3, 1) should equal 1 :", pgcd(3, 1)
+	print "bezout(57, 33) should equal (3, -4, 7) :", bezout(57, 33)
+	print "bezout(2, -4) should equal (2, -1, -1) :", bezout(2, -4)
--- utils.py
+++ utils.py
@@ -4,3 +4,15 @@ def pgcd(a, b):
 		a = b
 		b = r
 	return a
+
+def bezout(a, b):
+	(u0, u1) = (1, 0)
+	(v0, v1) = (0, 1)
+	while b != 0:
+		r = a % b
+		q = (a - r) / b
+		(a, b) = (b, r)
+		(u0, u1) = (u1, u0 - q*u1)
+		(v0, v1) = (v1, v0 - q*v1)
+	return (a, u0, v0) if a > 0 else (-a, -u0, -v0)
+ 
